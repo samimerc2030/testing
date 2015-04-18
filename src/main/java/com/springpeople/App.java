7b1532b92613f3e21ef6354a68780792d12ce423
swaminathan <swami@test.com>
2015-04-18 15:43:40 +0530

    change1

--- src/main/java/com/springpeople/App.java
+++ src/main/java/com/springpeople/App.java
@@ -1,13 +1,21 @@
 package com.springpeople;
 
 /**
- * Hello world!
+ * Hello world! this is App class
  *
  */
 public class App 
 {
+    /**Main method*/
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! -- withgithub" );
+        System.out.println( "Hello World! -- withgithub 1" );
     }
+
+/**sub method*/
+    public void sample()
+    {
+        System.out.println( "saminathan" );
+    }
+
 }
